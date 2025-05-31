09b51006fcfab2d3a4a01858a5d6f944de95c567
spex2024 <dercolbags@gmail.com>
2025-05-31 23:24:30 +0000

    unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -60,7 +60,9 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         with patch.object(
-            TestClass, "a_method", return_value=42
+            TestClass,
+            "a_method",
+            return_value=42
         ) as mock_method:
             obj = TestClass()
             self.assertEqual(obj.a_property(), 42)
