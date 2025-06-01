de22ada79ba407acadfe5f36b0b702f19e37ad16
spex2024 <dercolbags@gmail.com>
2025-06-01 08:28:35 +0000

    messaging app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,19 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
-router.register(r'messages', MessageViewSet)
+
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
 ]
