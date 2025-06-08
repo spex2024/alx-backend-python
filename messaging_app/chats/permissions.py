642f356104c1ecf4afb4cb2e6773192a9575649b
spex2024 <dercolbags@gmail.com>
2025-06-08 20:35:26 +0000

    auth and permissions

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -2,12 +2,18 @@ from rest_framework import permissions
 from .models import Conversation, Message
 
 class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Allows access only to authenticated users who are participants
+    of the conversation related to the object (Conversation or Message).
+    """
+
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        if isinstance(obj, Conversation):
-            return request.user in obj.participants.all()
-        if isinstance(obj, Message):
-            return request.user in obj.conversation.participants.all()
+        if request.method in ['PUT', 'PATCH', 'DELETE', 'GET', 'POST']:
+            if isinstance(obj, Conversation):
+                return request.user in obj.participants.all()
+            elif isinstance(obj, Message):
+                return request.user in obj.conversation.participants.all()
         return False
