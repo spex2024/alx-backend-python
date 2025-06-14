4354a69a1e2dfbbff4d33e472aabee4db5dfecc5
spex2024 <dercolbags@gmail.com>
2025-06-14 00:31:01 +0000

    Event Listeners (Signals), ORM & Advanced ORM Techniques

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,23 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.models import User
-from django.views.decorators.cache import cache_page
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from Django_Chat.Models.message import Message
 
 
-@cache_page(60)  # ✅ View caching
 @login_required
-def conversation_view(request, username):
-    sender = request.user  # ✅ Sender is request.user
-    receiver = get_object_or_404(User, username=username)
+def unread_messages_view(request):
+    # ✅ Use the custom manager + .only() explicitly
+    unread_messages = Message.unread.unread_for_user(request.user).only('id', 'sender', 'receiver', 'content', 'timestamp')
 
-    # ✅ Using Message.objects.filter + select_related
-    messages = Message.objects.filter(
-        sender=sender,
-        receiver=receiver
-    ).select_related('sender', 'receiver').order_by('-timestamp')
-
-    return render(request, 'messaging/conversation.html', {
-        'messages': messages,
-        'receiver': receiver,
+    return render(request, 'messaging/unread_messages.html', {
+        'unread_messages': unread_messages
     })
